90389d25ecd6d66d7af82f0c1edcddd887b755ae
JTNghia <19521899@gm.uit.edu.vn>
2021-06-27 11:06:28 +0700

    .

--- env/StockTradingEnv.py
+++ env/StockTradingEnv.py
@@ -136,7 +136,7 @@ class StockTradingEnv(gym.Env):
         # Set the current step to a random point within the data frame
         # self.current_step = random.randint(
         #     0, len(self.df.loc[:, 'Open'].values) - 6)
-        self.current_step = num_trains
+        self.current_step = self.num_trains
 
         return self._next_observation()
 
